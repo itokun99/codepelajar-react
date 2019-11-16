e5e1cb50f4eee6eaaccaa15959c07b9eae5dd410
Indrawan Lisanto <indrawan@aleph-labs.com>
2019-11-16 13:50:48 +0700

    add release folder

---
+++ release/build-1.0.0/precache-manifest.de4e47b908104520e9efd489c3308067.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba2bfc446ffedb1443d821d4b7f87629",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc70848ff6a26324ea71",
+    "url": "/static/css/main.a12f96e7.chunk.css"
+  },
+  {
+    "revision": "8453923829a3115890dd",
+    "url": "/static/js/2.5e982b26.chunk.js"
+  },
+  {
+    "revision": "cc70848ff6a26324ea71",
+    "url": "/static/js/main.922618e0.chunk.js"
+  },
+  {
+    "revision": "402810f515b519aa06ee",
+    "url": "/static/js/runtime-main.8e448b9b.js"
+  }
+]);
